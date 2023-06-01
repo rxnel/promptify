b48718a4653910a1db3be83c4c221b59eb487d53
Ronel A <ronel.aguilar@icloud.com>
2023-05-31 20:24:57 -0400

    adding temp faq

---
+++ app/faq/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      <h1>Placeholder FAQ Page</h1>
+      <p>This page is a placeholder for the FAQ page. It will be updated with the FAQ page</p>
+    </div>
+  );
+};
+
+export default page;
